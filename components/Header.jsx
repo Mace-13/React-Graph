0404759ca776c8be54d0aa1bc5375c40242e033b
Mandy Pletinckx <pletinckx.mandy@gmail.com>
2022-05-27 20:53:19 +0200

    front

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,39 +1,44 @@
 import React, {useEffect, useState } from 'react'
 import Link from 'next/link';
 
+import { getNavs } from '../services';
 
 const Header = () => {
     let [open, setopen] = useState(false)
 
   return (
-    <nav className="flex items-center justify-between pt-5">
+    <nav className="flex items-center justify-between pt-5 lightPink">
          <img src={open ? "./icon-close.svg" : "./icon-hamburger.svg"} className="md:hidden  fixed right-5 cursor-pointer z-20 top-6" onClick={() => setopen(!open)} />
          <div className='md:float-left block px-20 pb-10'>
-                <Link href="/">
-                    <span className='cursor-pointer title-font font-bold text-4xl text-white'>
-                        OHAYO 
-                    </span>
-                </Link>
-            </div>
+           <a href="/" className="">
+               <img src="./logoBlog.svg" className="mr-3 w-24 h-24" alt="Ohayo Logo" />
+            </a>
+         </div>
+         
+         
 
          <ul className={`bg-[#ffffff14] backdrop-blur-md  md:pl-10 pr-28 md:static pb-5 fixed duration-500 ease-linear top-0 md:h-auto h-screen z-10 ${!open ? 'right-[-100%] ' : 'right-0'}`}>
             <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
                <Link href="/">
-                  <a className="cursor-pointer text-color font-semibold inline-block md:py-5 py-3 ">Home</a>
+                  <a className="cursor-pointer text-color text-lg inline-block md:py-5 py-3 ">Home</a>
                </Link>        
             </li>
             <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
                <Link href="/blog/">
-                  <a className="cursor-pointer text-color font-semibold inline-block md:py-5 py-3 ">Blog</a>
+                  <a className="cursor-pointer text-color text-lg inline-block md:py-5 py-3 ">Blog</a>
                </Link>        
             </li>
             <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
                <Link href="/">
-                  <a className="cursor-pointer text-color font-semibold inline-block md:py-5 py-3 ">Gallerie</a>
+                  <a className="cursor-pointer text-color text-lg inline-block md:py-5 py-3 ">Galerie</a>
+               </Link>        
+            </li>
+            <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
+               <Link href="/">
+                  <a className="cursor-pointer text-color text-lg inline-block md:py-5 py-3 ">Contact</a>
                </Link>        
             </li>
              
-           
          </ul>
       </nav>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,29 @@
-import Head from 'next/head'
-import Link from 'next/link';
-import { PostCard, Categories, PostWidget } from '../components';
-import { getPosts } from '../services'
-import { FeaturedPosts } from '../sections';
+import Head from "next/head";
+import Link from "next/link";
+import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
+import { Interests, About } from "../sections";
 
-export default function Home({ posts}) {
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div>
       <Head>
         <title>React Graphql Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="mx-auto homebg rounded-xl shadow-md overflow-hidden mb-10 flex items-center justify-center">
-        <div className="homeBox shadow-lg rounded-lg p-10 max-w-md">
-          <h3 className='mb-5 text-1xl font-semibold'>
-            Hello Traveler !
-          </h3>
-          <p className='text-white mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non lacinia eros. Maecenas ut tempor elit, vitae mollis quam. In semper sem ut eros faucibus, eu rhoncus mi suscipit. Nunc faucibus aliquam nunc, sed bibendum leo suscipit semper. Pellentesque euismod mauris sed tincidunt vehicula. Fusce fringilla, velit et fermentum ullamcorper.</p>
-          <div className='text-center'>
-            <Link href="">
-              <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-red-200 text-sm font-medium rounded-full text-stone-500 px-4 py-2 cursor-pointer'>
-                Voir les articles
-              </span>
-            </Link>
-          </div>
-        </div>
+      <div className="homebg mb-10 flex justify-center shadow-md">
+        <h3 className="title-font py-10 text-4xl text-white mx-6">
+          “ Don’t listen to what they say, go see ”
+        </h3>
       </div>
-     
-      <FeaturedPosts />
+       
+     <Interests />
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const posts = (await getPosts()) || [] ;
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
   return {
     props: { posts },
   };
