b81047c527965dbcc87566df81006f39f8e46c2f
Mandy Pletinckx <pletinckx.mandy@gmail.com>
2022-06-29 13:11:45 +0200

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../components';
 
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return(
+    <>
+     {loading ? <div className="loadingImg flex mx-auto pt-12">
+      <img src="./loading.gif" alt="" />
+     </div> :
     <Layout>
+      
       <Component {...pageProps} />
+       
     </Layout>
+   }
+    </>
+    
   )
 
   }
